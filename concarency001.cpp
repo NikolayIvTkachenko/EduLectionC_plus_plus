7e370c7723294f96872aace8fe20e337a42cba6f
NikolayTkachenko <tkachenni@mail.ru>
2024-10-29 00:19:07 +0300

    update 8

--- concarency001.cpp
+++ concarency001.cpp
@@ -2,6 +2,10 @@
 #include <thread>
 //#include <mingw.thread.h>
 
+//#include <pthread.h>
+
+using namespace std;
+
 void hello()
 {
     std::cout << "Hello Concurrent Word\n";
@@ -9,7 +13,7 @@ void hello()
 
 void func001()
 {
-    std::thread t(hello);
+    thread t(hello);
     t.join();
 }
 
@@ -17,3 +21,22 @@ int main()
 {
     func001();
 }
+
+//https://qna.habr.com/q/547925
+//$ cat > thr.cpp
+//#include <thread>
+//using namespace std;
+//void doIt(){
+  //do nothing
+//}
+//int main(){
+//  thread t(doIt);
+//  t.detach();
+//  return 0;
+//}
+//$ g++ -std=c++11 thr.cpp -o thr
+///tmp/ccVyziLM.o: In function `std::thread::thread<void (&)()>(void (&)())':
+//thr.cpp:(.text._ZNSt6threadC2IRFvvEJEEEOT_DpOT0_[_ZNSt6threadC5IRFvvEJEEEOT_DpOT0_]+0x20): undefined reference to `pthread_create'//
+//collect2: error: ld returned 1 exit status
+
+//g++ -std=c++11 concarency001.cpp -o thr -pthread -std=c++11
